06906ea7b3ded4cde4a2018b457717e4507f1f8c
Andres Gaviria T <44479766+agavariat@users.noreply.github.com>
2021-02-26 13:55:15 -0500

    Add files via upload

---
+++ models/crm_lead.py
@@ -0,0 +1,47 @@
+# Copyright 2015 Antiun Ingeniería, S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class Lead(models.Model):
+    _inherit = "crm.lead"
+
+    vat = fields.Char(
+        string="TIN",
+        help="Tax Identification Number. The first 2 characters are the "
+             "country code.")
+
+    tamemp = fields.Selection(
+        [
+            ('1', 'Grande'),
+            ('2', 'Mediana'),
+            ('3', 'Pequeña'),
+            ('4', 'Empresa Personal'),
+        ], "Tamaño de Empresa",
+    )
+    linea = fields.Many2many('model.manipulate.many2many', string="Linea de producto")
+    sec = fields.Selection(
+        [
+            ('1', 'Almacenes por Departamento'),
+            ('2', 'Belleza y Cosmeticas'),
+            ('3', 'Alimentos'),
+            ('4', 'Bebidas'),
+        ], "Sector",
+    )
+    client = fields.Selection(
+        [
+            ('1', 'Almacenes por Departamento'),
+            ('2', 'Belleza y Cosmeticas'),
+            ('3', 'Alimentos'),
+            ('4', 'Bebidas'),
+        ], "Clase de Cliente",
+    )
+    com = fields.Selection(
+        [
+            ('1', 'Almacenes por Departamento'),
+            ('2', 'Belleza y Cosmeticas'),
+            ('3', 'Alimentos'),
+            ('4', 'Bebidas'),
+        ], "Como se contacto",
+    )
